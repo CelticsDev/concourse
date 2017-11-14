6e9dd6aed4a474be4073fcde75b6df2f952035d3
kylepg <kylepgeorge@gmail.com>
2017-11-14 17:28:03 -0500

    EOD 11-14-17

--- src/js/scratch.js
+++ src/js/scratch.js
@@ -5,3 +5,50 @@
             jQuery("div[data-pid='" + roster[i].pid + "'] .averages-season").html('<td><p>' + teamStats[roster[i].pid].avg.gp + '</p></td><td><p>' + teamStats[roster[i].pid].avg.pts + '</p></td><td><p>' + teamStats[roster[i].pid].avg.reb + '</p></td><td><p>' + teamStats[roster[i].pid].avg.ast + '</p></td>')
         }*/
 jQuery('.player-spotlight .player-wrap:nth-child(1)').addClass("active");
+
+
+
+// LOAD ROSTER (AND GET YEAR)
+/*    jQuery.ajax({
+        url: 'http://data.nba.com/data/v2015/json/mobile_teams/nba/' + rosterYear + '/teams/celtics_roster.json',
+        async: false,
+        success: function(data) {
+            roster = data.t.pl;
+        },
+        error: function() {
+            rosterYear--;
+            jQuery.ajax({
+                url: "http://data.nba.com/data/v2015/json/mobile_teams/nba/" + rosterYear + "/teams/celtics_roster.json",
+                async: false,
+                success: function(data) {
+                    roster = data.t.pl;
+                }
+            });
+        }
+    });
+    // LOAD TEAM STATS
+    jQuery.ajax({
+        url: 'http://data.nba.com/data/v2015/json/mobile_teams/nba/' + rosterYear + '/teams/celtics/player_averages_02.json',
+        async: false,
+        success: function(data) {
+            for (var i = 0; i < data.tpsts.pl.length; i++) {
+                teamStats[data.tpsts.pl[i].pid] = data.tpsts.pl[i]
+            }
+        },
+        error: function() {
+            rosterYear--;
+            jQuery.ajax({
+                url: 'http://data.nba.com/data/v2015/json/mobile_teams/nba/' + rosterYear + '/teams/celtics/player_averages_02.json',
+                async: false,
+                success: function(data) {
+                    for (var i = 0; i < data.tpsts.pl.length; i++) {
+                        teamStats[data.tpsts.pl[i].pid] = data.tpsts.pl[i]
+                    }
+                }
+            });
+        }
+    });*/
+bioData = '';
+/*	jQuery.getJSON('http://io.cnn.net/nba/nba/.element/media/2.0/teamsites/celtics/misc/bioObj.json?', function(data) {
+	    bioData = data;
+	});*/
